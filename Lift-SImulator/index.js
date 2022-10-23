2b37f77a8e44c694bc73c06154a1138501ab2cc3
PRANKUR PANDEY <pprankur@gmail.com>
2022-10-23 18:14:45 +0530

    fix :  added the lifts and floors dynamicaly

--- Lift-SImulator/index.js
+++ Lift-SImulator/index.js
@@ -1,37 +1,45 @@
-// Steps
-// Take input for floors adn lifts from the user and dynamically
-// generate lifts and floors by default all lifts will be at the ground floor
+/*
+Algorithm steps : 
+1 - Take input and and make lift and floors based on  the input value
+2- Make lifts at the first floor lifts should be dynamic in nature 
+3- Make up/down button such that the first floor should have only Up buttona nd
+top floor should have only down buttona while other should have both up and down \
+button
+4-  simulate the lifts like when clicked on the top button the lift  which is free should 
+be move to the floor from where it is being called vice-versa for down button
+ */
 
 const floorsInput = document.getElementById("input_floors");
 const liftsInput = document.getElementById("input_lifts");
 const submitForm = document.getElementById("form");
+const floors = [];
 
-// Create Floors
-
-function createFloors(e) {
-  e.preventDefault();
-  const floors = [];
-
-  // acces the div and user input to make floors
-  const createFloors = document.getElementById("floorsAndLifts");
-  for (i = 1; i <= floorsInput.value; i++) {
-    const newFloors = document.createElement("HR");
-    newFloors.style.margin = "2rem";
-    newFloors.style.display = "flex";
-    newFloors.style.justifyContent = "space-around";
-    createFloors.appendChild(newFloors);
-    floors.push(newFloors);
-  }
-
-  //  make lifts
-
-  for (j = 1; j <= liftsInput.value; j++) {
-    const newLifts = document.createElement("DIV");
-    newLifts.style.width = "20px";
-    newLifts.style.height = "40px";
-    newLifts.style.backgroundColor = "red";
-    floors[floors.length - 1].appendChild(newLifts);
+// make floors
+function makeFloors() {
+  for (let i = 1; i <= floorsInput.value; i++) {
+    const makeFloors = document.createElement("div");
+    makeFloors.setAttribute("class", "makeFloors");
+    const floorAndLifts = document.getElementById("floorsAndLifts");
+    floorAndLifts.appendChild(makeFloors);
+    floors.push(makeFloors);
   }
 }
 
-submitForm.addEventListener("click", createFloors);
+// make lifts
+function makeLifts() {
+  for (let i = 1; i <= floorsInput.value; i++) {
+    const makeLifts = document.createElement("div");
+    makeLifts.setAttribute("class", "makeLifts");
+    floors[floors.length - 1].appendChild(makeLifts);
+  }
+}
+// make buttons
+function makeMovingButtons() {}
+
+function simulateLifts(e) {
+  e.preventDefault();
+  makeFloors();
+  makeLifts();
+}
+
+submitForm.addEventListener("click", simulateLifts);
